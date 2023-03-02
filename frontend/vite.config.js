836e113cd51743e53ed8087a465a50822ee6ab08
Ignacio Garcia <nachogarcia003@gmail.com>
2023-03-02 10:46:00 -0300

    details

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["../frontend/node_modules/react-icons/bs"],
+      external: ["./react-icons/Bs"],
     },
   },
 });
