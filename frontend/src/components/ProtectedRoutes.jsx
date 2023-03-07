9087194dea797195abe25f22ef5bcade984311c6
Ignacio Garcia <48738658+iegarcia@users.noreply.github.com>
2023-03-07 13:48:36 -0300

    Delete frontend directory

--- frontend/src/components/ProtectedRoutes.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-export function ProtectedRoutes({ children }) {
-  const token = localStorage.getItem("token");
-  if (token === null) return <Navigate to="/login" />;
-
-  return <>{children} </>;
-}
